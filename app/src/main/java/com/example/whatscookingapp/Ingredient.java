e95d5f316034b30f204c4846c042edee149b6729
sdabrowski <sd84470n@pace.edu>
2020-05-06 07:07:13 -0400

    Add Recipe and Ingredient classes
    Adding Recipe and Ingredient classes from the add data app. Data in the database should be stored in the same format as objects of these two classes

---
+++ app/src/main/java/com/example/whatscookingapp/Ingredient.java
@@ -0,0 +1,45 @@
+package com.example.whatscookingapp;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ingredient {
+    private String ingredientName;
+    private String ingredientURL;
+    private List<String> dependentRecipes;
+    public Ingredient(){}
+    public Ingredient(String IngredientName, String IngredientURL, ArrayList<String> DependentRecipes){
+        ingredientName = IngredientName;
+        ingredientURL = IngredientURL;
+        dependentRecipes = DependentRecipes;
+    }
+
+
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public void setIngredientURL(String ingredientURL) {
+        this.ingredientURL = ingredientURL;
+    }
+
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public String getIngredientURL() {
+        return ingredientURL;
+    }
+
+
+    public List<String> getDependentRecipes() {
+        return dependentRecipes;
+    }
+
+    public void setDependentRecipes(List<String> dependentRecipes) {
+        this.dependentRecipes = dependentRecipes;
+    }
+}
+
---
+++ app/src/main/java/com/example/whatscookingapp/Recipe.java
@@ -0,0 +1,75 @@
+package com.example.whatscookingapp;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Recipe {
+    private String recipeName;
+    private String recipeInstruction;
+    private String recipeURL;
+    private HashMap<String, String> recipeIngredients = new HashMap<String, String>();
+    private int mealType; // 0 = Breakfast, 1 = Lunch, 2 = Dinner, 3 = Snacks
+    public Recipe(){}
+    public Recipe(String RecipeName, String RecipeInstruction, String RecipeURL, int MealType, ArrayList<String> RecipeIngredients){
+        recipeName = RecipeName;
+        recipeInstruction = RecipeInstruction;
+        recipeURL = RecipeURL;
+        mealType = MealType;
+        for (int i = 0; i< RecipeIngredients.size(); i++){
+            String index = String.valueOf(i);
+            recipeIngredients.put(index, RecipeIngredients.get(i));
+        }
+    }
+    public Recipe(String RecipeName, String RecipeInstruction, String RecipeURL, int MealType, HashMap<String, String> RecipeIngredients){
+        recipeName = RecipeName;
+        recipeInstruction = RecipeInstruction;
+        recipeURL = RecipeURL;
+        mealType = MealType;
+        recipeIngredients = RecipeIngredients;
+
+    }
+    public void setMealType(int mealType) {
+        this.mealType = mealType;
+    }
+
+
+
+    public void setRecipeInstruction(String recipeInstruction) {
+        this.recipeInstruction = recipeInstruction;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public void setRecipeURL(String recipeURL) {
+        this.recipeURL = recipeURL;
+    }
+
+    public String getRecipeInstruction() {
+        return recipeInstruction;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public String getRecipeURL() {
+        return recipeURL;
+    }
+
+    public int getMealType() {
+        return mealType;
+    }
+
+    public HashMap<String, String> getRecipeIngredients() {
+        return recipeIngredients;
+    }
+
+    public void setRecipeIngredients(HashMap<String, String> recipeIngredients) {
+        this.recipeIngredients = recipeIngredients;
+    }
+}
